Project-PixelStar/packages_apps_Settings

Settings: Implement the new Font Picker

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -205,7 +205,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.livedisplay.LiveDisplaySettings;
 import com.star.field.StarfieldDashboard;
 import com.star.field.category.Theme;
-import com.star.field.fragments.FontsPicker;
+import com.star.field.fragments.themes.fonts.FontPickerPreview;
 
 public class SettingsGateway {
 
@@ -400,7 +400,7 @@ public class SettingsGateway {
             WirelessDebuggingFragment.class.getName(),
             StarfieldDashboard.class.getName(),
             Theme.class.getName(),
-            FontsPicker.class.getName(),
+            FontPickerPreview.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -449,7 +449,7 @@ public class SettingsGateway {
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.WirelessDebuggingActivity.class.getName(),
             Settings.StarfieldSettingsActivity.class.getName(),
-            Settings.FontsPickerActivity.class.getName(),
+            Settings.FontPickerPreviewActivity.class.getName(),
             Settings.ThemeActivity.class.getName(),
     };
 }

SerasaOS/android_packages_apps_Settings

Change-Id: Ia3e95712a222f4b44b830265ecd2a6a53cbf50bc

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -205,7 +205,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.livedisplay.LiveDisplaySettings;
 import com.morbid.labs.MorbidlabsDashboard;
 import com.morbid.labs.category.Theme;
-import com.morbid.labs.fragments.FontsPicker;
+import com.morbid.labs.fragments.themes.fonts.FontPickerPreview;
 
 public class SettingsGateway {
 
@@ -400,7 +400,7 @@ public class SettingsGateway {
             WirelessDebuggingFragment.class.getName(),
             MorbidlabsDashboard.class.getName(),
             Theme.class.getName(),
-            FontsPicker.class.getName(),
+            FontPickerPreview.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -449,7 +449,7 @@ public class SettingsGateway {
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.WirelessDebuggingActivity.class.getName(),
             Settings.MorbidlabsSettingsActivity.class.getName(),
-            Settings.FontsPickerActivity.class.getName(),
+            Settings.FontPickerPreviewActivity.class.getName(),
             Settings.ThemeActivity.class.getName(),
     };
 }